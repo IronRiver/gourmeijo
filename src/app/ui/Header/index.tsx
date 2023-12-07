814890d48e5200e8d481f43334e3baf2acdc81e1
IronRiver <ironriver2003@gmail.com>
2023-12-07 18:59:18 +0900

    refactor: Remove　custom　Typography
    分かりにくかったので自作のタイポグラフィーコンポーネントを削除しました

--- src/app/ui/Header/index.tsx
+++ src/app/ui/Header/index.tsx
@@ -1,35 +1,35 @@
 import { Menu } from "@mui/icons-material";
-import { AppBar, Box, IconButton, Toolbar } from "@mui/material";
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
 
 import { Search } from "../Search";
-import { Typography } from "../Typography";
 
 export interface HeaderProps {}
 
 export function Header(props: HeaderProps) {
   return (
-    <AppBar position="sticky">
-      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="open drawer"
-          sx={{ mr: 2 }}
-        >
-          <Menu />
-        </IconButton>
-        <Typography
-          role="title"
-          size="large"
-          noWrap
-          component="div"
-          sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
-        >
-          ぐるMeijo
-        </Typography>
-        <Search />
-      </Toolbar>
-    </AppBar>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="sticky">
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            ぐるMeijo
+          </Typography>
+          <Search />
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
--- src/app/ui/Search/index.tsx
+++ src/app/ui/Search/index.tsx
@@ -1,11 +1,41 @@
+"use client";
+
 import { Search as SearchIcon } from "@mui/icons-material";
+import { styled, alpha } from "@mui/material/styles";
 
 export interface SearchProps {}
 
+const SearchNav = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
 export function Search(props: SearchProps) {
   return (
-    <nav>
-      <SearchIcon />
-    </nav>
+    <SearchNav>
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+    </SearchNav>
   );
 }
