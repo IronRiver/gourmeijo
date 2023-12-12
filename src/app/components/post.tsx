a7945e9f10b33f105313f448c0d12e1e52ca49e5
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2023-12-12 13:24:51 +0900

    Update post.tsx
    投稿の削除機能を実装しました

--- src/app/components/post.tsx
+++ src/app/components/post.tsx
@@ -3,12 +3,12 @@ import {
   db,
   collection,
   getDocs,
-  deleteDoc,
-  doc,
-  auth,
   updateDoc,
   arrayUnion,
   arrayRemove,
+  doc,
+  auth,
+  deleteDoc,
 } from "../firebase";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
@@ -21,6 +21,7 @@ import Avatar from "@mui/material/Avatar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Rating from "@mui/material/Rating";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 interface PostData {
   id: string;
@@ -33,11 +34,12 @@ interface PostData {
   rating: number;
   shopName: string;
   likes: string[];
+  userName: string;
 }
 
 const Post: React.FC = () => {
   const [postData, setPostData] = useState<PostData[]>([]);
-  const currentUser = auth.currentUser; // 現在のユーザーを取得
+  const currentUser = auth.currentUser;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -56,6 +58,7 @@ const Post: React.FC = () => {
             review,
             shopName,
             likes,
+            userId,
           } = doc.data();
 
           data.push({
@@ -68,11 +71,12 @@ const Post: React.FC = () => {
               "不明なデータ",
             imageUrl: imageUrl || "",
             content: content || "Example Content",
-            userId: userName || "Unknown User",
+            userId: userId || "Unknown User",
             review: review || "レビューがありません",
             rating: rating || 0,
             shopName: shopName || "Unknown Shop",
             likes: likes || [],
+            userName: userName || "ユーザー名無し",
           } as PostData);
         });
 
@@ -83,7 +87,7 @@ const Post: React.FC = () => {
     };
 
     fetchData();
-  }, [currentUser]); // currentUser が変更されたときに再実行する
+  }, []); // 最初のマウント時にのみデータを取得する
 
   const getTotalLikes = (postId: string): number => {
     const post = postData.find((p) => p.id === postId);
@@ -92,89 +96,136 @@ const Post: React.FC = () => {
 
   const handleLikeToggle = async (postId: string) => {
     try {
-      const postRef = doc(db, "reviews", postId);
+      const postIndex = postData.findIndex((p) => p.id === postId);
 
-      // いいねがすでにされているか確認
-      const liked = postData
-        .find((post) => post.id === postId)
-        ?.likes.includes(currentUser?.uid || "");
+      if (postIndex !== -1) {
+        const post = postData[postIndex];
+        const liked = post.likes.includes(currentUser?.uid || "");
 
-      if (liked) {
-        // いいねを取り消す
-        await updateDoc(postRef, {
-          likes: arrayRemove(currentUser?.uid),
-        });
-      } else {
-        // いいねを追加する
-        await updateDoc(postRef, {
-          likes: arrayUnion(currentUser?.uid),
-        });
+        const postRef = doc(db, "reviews", postId);
+
+        if (liked) {
+          // いいねを取り消す
+          await updateDoc(postRef, {
+            likes: arrayRemove(currentUser?.uid),
+          });
+        } else {
+          // いいねを追加する
+          await updateDoc(postRef, {
+            likes: arrayUnion(currentUser?.uid),
+          });
+        }
+
+        // ローカルのpostDataを更新
+        const updatedPostData = [...postData];
+        updatedPostData[postIndex] = {
+          ...post,
+          likes: liked
+            ? post.likes.filter((like) => like !== currentUser?.uid)
+            : [...post.likes, currentUser?.uid || ""],
+        };
+
+        setPostData(updatedPostData);
       }
-
-      // データを再取得して更新
-      fetchData();
     } catch (error) {
       console.error("Error toggling like:", error);
     }
   };
+  const handleDeletePost = async (postId: string) => {
+    try {
+      const postRef = doc(db, "reviews", postId);
+      await deleteDoc(postRef);
 
+      // ローカルのpostDataから削除
+      const updatedPostData = postData.filter((post) => post.id !== postId);
+      setPostData(updatedPostData);
+    } catch (error) {
+      console.error("Error deleting post:", error);
+    }
+  };
+  useEffect(() => {
+    console.log("Current User ID:", currentUser?.uid);
+  }, [currentUser?.uid]);
   return (
     <>
-      {postData.map((post) => (
-        <Card key={post.id} sx={{ margin: 5 }}>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
-                {post.userId.charAt(0)}
-              </Avatar>
-            }
-            action={<IconButton aria-label="settings"></IconButton>}
-            title={`投稿者: ${post.userId}`}
-            subheader={post.date}
-          />
-
-          <CardMedia
-            component="img"
-            height="194"
-            image={post.imageUrl}
-            alt="画像なし"
-          />
-
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              {post.review}
-            </Typography>
-            <Typography variant="subtitle2" color="text.secondary">
-              店名: {post.shopName}
-            </Typography>
-            <Rating
-              name={`rating-${post.id}`}
-              value={post.rating}
-              precision={0.5}
-              readOnly
+      {postData.map((post) => {
+        console.log("currentUser UID:", currentUser?.uid);
+        console.log("post userId:", post.userId);
+        console.log("Match:", currentUser?.uid === post.userId);
+        return (
+          <Card key={post.id} sx={{ margin: 5 }}>
+            <CardHeader
+              avatar={
+                <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+                  {post.userId.charAt(0)}
+                </Avatar>
+              }
+              action={
+                currentUser?.uid === post.userId ? (
+                  <IconButton
+                    aria-label="delete"
+                    onClick={() => handleDeletePost(post.id)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                ) : null
+              }
+              title={`投稿者: ${post.userName}`}
+              subheader={post.date}
             />
-          </CardContent>
 
-          <CardActions disableSpacing>
-            <IconButton
-              aria-label="add to favorites"
-              onClick={() => handleLikeToggle(post.id)}
-            >
-              {post.likes.includes(currentUser?.uid || "") ? (
-                <FavoriteIcon sx={{ color: "red" }} />
-              ) : (
-                <FavoriteBorderIcon />
+            <CardMedia
+              component="img"
+              height="194"
+              className="w-auto max-h-[350px] mx-auto my-auto"
+              image={post.imageUrl}
+              alt=""
+            />
+            {!post.imageUrl && (
+              <span className="block mx-auto text-center border-b">
+                画像なし
+              </span>
+            )}
+
+            <CardContent>
+              <Typography variant="subtitle2" color="text.secondary">
+                店名: {post.shopName}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                レビュー：{post.review}
+              </Typography>
+
+              <Rating
+                name={`rating-${post.id}`}
+                value={post.rating}
+                precision={0.5}
+                readOnly
+              />
+            </CardContent>
+            <CardActions disableSpacing>
+              {currentUser?.uid === post.userId && (
+                <IconButton
+                  aria-label="delete"
+                  onClick={() => handleDeletePost(post.id)}
+                ></IconButton>
               )}
-            </IconButton>
-            <span>{getTotalLikes(post.id)}</span>
-          </CardActions>
-        </Card>
-      ))}
+              <IconButton
+                aria-label="add to favorites"
+                onClick={() => handleLikeToggle(post.id)}
+              >
+                {post.likes.includes(currentUser?.uid || "") ? (
+                  <FavoriteIcon sx={{ color: "red" }} />
+                ) : (
+                  <FavoriteBorderIcon />
+                )}
+              </IconButton>
+              <span>{getTotalLikes(post.id)}</span>
+            </CardActions>
+          </Card>
+        );
+      })}
     </>
   );
 };
 
 export default Post;
-function fetchData() {
-  throw new Error("Function not implemented.");
-}
